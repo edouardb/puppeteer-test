919bd0a6d4e69b6c2381d06e51f7cbade9fd1773
David Vismans <david@vismans.net>
2023-05-27 21:17:21 +0200

    Temp

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     console.log("Starting headless browser...");
-	const browser = await puppeteer.launch({headless: "new"});
-	const page = await browser.newPage();
-	await page.goto('https://www.google.com/');
+	// const browser = await puppeteer.launch({headless: "new"});
+	// const page = await browser.newPage();
+	// await page.goto('https://www.google.com/');
 	
-	await browser.close();
+	// await browser.close();
     console.log("Worked!");
 })();
