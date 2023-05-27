3df24212aa56b5e7eca60d3cdb36b9945cb8847a
David Vismans <david@vismans.net>
2023-05-27 21:40:38 +0200

    Testing

--- index.js
+++ index.js
@@ -2,17 +2,10 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     console.log("Starting headless browser...");
-	// const browser = await puppeteer.launch({headless: "new"});
-	// const page = await browser.newPage();
-	// await page.goto('https://www.google.com/');
+	const browser = await puppeteer.launch({headless: "new"});
+	const page = await browser.newPage();
+	await page.goto('https://www.google.com/');
 	
-	// await browser.close();
+	await browser.close();
     console.log("Worked!");
-
-    var interval = setInterval(function(){ 
-        console.log('Hello World'); 
-      }, 2000);
-      setTimeout(function() { 
-        clearInterval(interval); 
-      }, 10000);
 })();
