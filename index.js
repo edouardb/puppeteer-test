9ea723f18b3ed18674316357d1fbb5f65a41d4f0
Edouard Bonlieu <ebonlieu@gmail.com>
2023-05-29 13:09:30 +0200

    Add Dockerfile

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
-    console.log("Starting headless browser...");
-	const browser = await puppeteer.launch({headless: "new"});
-	const page = await browser.newPage();
-	await page.goto('https://www.google.com/');
-	
-	await browser.close();
-    console.log("Worked!");
+  console.log("Starting headless browser...");
+  const browser = await puppeteer.launch({
+    headless: "new",
+    executablePath: process.env.CHROMIUM_PATH,
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.google.com/");
+
+  await browser.close();
+  console.log("Worked!");
 })();
